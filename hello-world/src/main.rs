9c8454cd490e32084b9751b85b48f909ce95cab6
zowietao <zowietao@gmail.com>
2023-06-29 00:30:28 +0800

    chore: variable, mutable, shadow number

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -6,17 +6,17 @@ macro_rules! println {
 
 macro_rules! print {
     ($($arg:tt)*) => ({
-        // actually print logic 
+        // actually print logic
         std::io::Write::write_fmt(&mut std::io::stdout(), format_args!($($arg)*)).unwrap();
     });
 }
 
-struct User{
-    name:String
+struct User {
+    name: String,
 }
 
 impl User {
-    fn hello(){
+    fn hello() {
         println!("hello world");
     }
     fn say(&self) {
@@ -24,16 +24,53 @@ impl User {
     }
 }
 
-
 fn main() {
-    let user =  User{
-        name: String::from("hello user")
+    let user = User {
+        name: String::from("hello user"),
     };
     User::hello();
     user.say();
 
+    println!(
+        "The first letter of the English alphabet is {} and the last letter is {}.",
+        'A', 'Z'
+    );
 
-    println!("The first letter of the English alphabet is {} and the last letter is {}.", 'A', 'Z');
-    
+    // VARIABLE
+    // Declare a variable
+    let a_number: i32;
+
+    // Declare a second variable and bind the value
+    let a_word: &str = "Ten";
+
+    // Bind a value to the first variable
+    a_number = 10;
+
+    println!("The number is {}.", a_number);
+    println!("The word is {}.", a_word);
+
+    // mutable and immutable
+    // a_number = 20
+
+    let mut b_number = 10;
+
+    println!("The number b is {}.", b_number);
+
+    b_number = 20;
+    println!("Now the number b is {}.", b_number);
+
+    // variable hidden
+
+    // Declare first variable binding with name "shadow_num"
+    let shadow_num: i32 = 5;
+
+    // Declare second variable binding, shadows existing variable "shadow_num"
+    let shadow_num: i32 = shadow_num + 5;
+
+    // Declare third variable binding, shadows second binding of variable "shadow_num"
+    let shadow_num: i32 = shadow_num * 2;
+
+    println!("The shadow_num is {}.", shadow_num);
+
     todo!("Display the message by using the println!() macro");
 }
