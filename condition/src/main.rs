d8c2f13b4e361ce373ced35cee280d6f30c3e0d0
zowietao <zowietao@gmail.com>
2023-07-01 01:00:31 +0800

    chore: format and unit test

--- condition/src/main.rs
+++ condition/src/main.rs
@@ -1,4 +1,12 @@
+use std::vec;
+
 fn main() {
+    array_test();
+
+    vector_test();
+}
+
+fn array_test() {
     // Declare array, initialize all values, compiler infers length = 7
     let days: [&str; 7] = [
         "Sunday",
@@ -25,7 +33,9 @@ fn main() {
         "bytes: {:?} \n first: {} \n second: {}",
         bytes, first, second
     );
+}
 
+fn vector_test() {
     let three_nums = vec![15, 3, 46];
     println!("Initial vector: {:?}", three_nums);
 
@@ -64,4 +74,97 @@ fn main() {
     // Access vector with out-of-bounds index
     let beyond = index_vec[10];
     println!("{}", beyond);
+}
+
+fn car_factory_test() {
+    #[derive(PartialEq, Debug)]
+    // Declare Car struct to describe vehicle with four named fields
+    struct Car {
+        color: String,
+        motor: Transmission,
+        roof: bool,
+        age: (Age, u32),
+    }
+
+    #[derive(PartialEq, Debug)]
+    // Declare enum for Car transmission type
+    enum Transmission {
+        Manual,
+        SemiAuto,
+        Automatic,
+    }
+
+    #[derive(PartialEq, Debug)]
+    enum Age {
+        New,
+        Used,
+    }
+
+    // Get the car quality by testing the value of the input argument
+    // - miles (u32)
+    // Create a tuple for the car quality with the Age ("New" or "Used") and mileage
+    // Return a tuple with the arrow `->` syntax
+    fn car_quality(miles: u32) -> (Age, u32) {
+        // Declare and initialize the return tuple value
+        // For a new car, set the miles to 0
+        let car_type: Age = if miles == 0 { Age::New } else { Age::Used };
+
+        let quality: (Age, u32) = (car_type, miles);
+
+        // Return the completed tuple to the caller
+        return quality;
+    }
+
+    // Build a new "Car" using the values of four input arguments
+    // - color (String)
+    // - motor (Transmission enum)
+    // - roof (boolean, true if the car has a hard top roof)
+    // - miles (u32)
+    // Call the car_quality(miles) function to get the car age
+    // Return an instance of a "Car" struct with the arrow `->` syntax
+    fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
+        // Create a new "Car" instance as requested
+        // - Bind first three fields to values of input arguments
+        // Corrected code: "mileage" is replaced with "age"
+        // Corrected code: Bind "age" to tuple returned from car_quality(miles)
+        Car {
+            color: color,
+            motor: motor,
+            roof: roof,
+            age: car_quality(miles),
+        }
+    }
+
+    let colors = ["Blue", "Green", "Red", "Silver"];
+
+    let mut car: Car;
+    let mut engine = Transmission::Manual;
+
+    //////////////////////////////////////////////////
+
+    // Order 3 cars, one car for each type of transmission
+    // Corrected code: Index into `colors` array and vary color for the orders
+
+    // Car order #1: New, Manual, Hard top
+    car = car_factory(String::from(colors[0]), engine, true, 0);
+    println!(
+        "Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #2: New, Semi-automatic, Convertible
+    engine = Transmission::SemiAuto;
+    car = car_factory(String::from(colors[1]), engine, false, 100);
+    println!(
+        "Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #3: New, Automatic, Hard top
+    engine = Transmission::Automatic;
+    car = car_factory(String::from(colors[2]), engine, true, 200);
+    println!(
+        "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
 }
