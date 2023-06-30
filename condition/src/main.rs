4ae39ad93db49d4f2874674bbf61879012f7676d
zowietao <zowietao@gmail.com>
2023-07-01 00:21:18 +0800

    chore: access vector

--- condition/src/main.rs
+++ condition/src/main.rs
@@ -51,4 +51,17 @@ fn main() {
     println!("Pop off: {:?}", fruit.pop());
     println!("Pop off: {:?}", fruit.pop());
     println!("Fruits: {:?}", fruit);
+
+    // Declare vector, initialize with three values
+    let mut index_vec = vec![15, 3, 46];
+    let three = index_vec[1];
+    println!("Vector: {:?}, three = {}", index_vec, three);
+
+    // Add 5 to the value at index 1, which is 5 + 3 = 8
+    index_vec[1] = index_vec[1] + 5;
+    println!("Vector: {:?}", index_vec);
+
+    // Access vector with out-of-bounds index
+    let beyond = index_vec[10];
+    println!("{}", beyond);
 }
