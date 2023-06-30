40400a8b5fdf8591bce33e5bfd0ef8857f35c5ff
zowietao <zowietao@gmail.com>
2023-06-30 21:43:03 +0800

    feat: enum

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -49,6 +49,7 @@ impl fmt::Display for MyString {
     }
 }
 
+// interface like
 trait ToMyString {
     fn to_my_string(&self) -> MyString;
 }
@@ -198,6 +199,63 @@ fn main() {
     println!(
         "{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}",
         user_2.name, user_2.level, user_2.remote, mark_2.0, mark_2.1, mark_2.2, mark_2.3, mark_2.4
+    );
+
+    // enum WebEvent {
+    //     // An enum variant can be like a unit struct without fields or data types
+    //     WELoad,
+    //     // An enum variant can be like a tuple struct with data types but no named fields
+    //     WEKeys(String, char),
+    //     // An enum variant can be like a classic struct with named fields and their data types
+    //     WEClick { x: i64, y: i64 },
+    // }
+
+    // Define a tuple struct
+    #[derive(Debug)]
+    struct KeyPress(String, char);
+
+    // Define a classic struct
+    #[derive(Debug)]
+    struct MouseClick {
+        x: i64,
+        y: i64,
+    }
+
+    // Define the WebEvent enum variants to use the data from the structs
+    // and a boolean type for the page Load variant
+    #[derive(Debug)]
+    enum WebEvent {
+        WELoad(bool),
+        WEClick(MouseClick),
+        WEKeys(KeyPress),
+    }
+
+    // Instantiate a MouseClick struct and bind the coordinate values
+    let click: MouseClick = MouseClick { x: 100, y: 250 };
+    println!("Mouse click location: {}, {}", click.x, click.y);
+
+    // Instantiate a KeyPress tuple and bind the key values
+    let keys: KeyPress = KeyPress(String::from("Ctrl+"), 'N');
+    println!("\nKeys pressed: {}{}", keys.0, keys.1);
+
+    // Instantiate WebEvent enum variants
+    // Set the boolean page Load value to true
+    let we_load: WebEvent = WebEvent::WELoad(true);
+    // Set the WEClick variant to use the data in the click struct
+    let we_click: WebEvent = WebEvent::WEClick(click);
+    // Set the WEKeys variant to use the data in the keys tuple
+    let we_key: WebEvent = WebEvent::WEKeys(keys);
+
+    // Print the values in the WebEvent enum variants
+    // Use the {:#?} syntax to display the enum structure and data in a readable form
+    println!(
+        "\nWebEvent enum structure: \n\n {:#?} \n\n {:#?} \n\n {:#?}",
+        we_load, we_click, we_key
+    );
+
+    println!(
+        "\nWebEvent enum structure: use {{:?}} \n\n {:?} \n\n {:?} \n\n {:?}",
+        we_load, we_click, we_key
     );
 
     todo!("Display the message by using the println!() macro");
