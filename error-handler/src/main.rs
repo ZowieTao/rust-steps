bd2e306647970716db7f6210d89e3be152c8f3a9
zowie <zowietao@gmail.com>
2023-07-03 14:28:23 +0800

    chore: person test

--- error-handler/src/main.rs
+++ error-handler/src/main.rs
@@ -22,6 +22,8 @@ fn main() {
     if_let_test();
 
     unwrap_expect();
+
+    person_test();
 }
 
 fn match_test() {
@@ -64,6 +66,58 @@ fn unwrap_expect() {
     // b.expect("fruits are healthy"); // panics with `fruits are healthy`
 
     assert_eq!(Some("dog").unwrap_or("cat"), "dog");
-    assert_eq!(Some("doggy").unwrap_or("cat"), "dog");
+    // assert_eq!(Some("doggy").unwrap_or("cat"), "dog");
     assert_eq!(None.unwrap_or("cat"), "cat"); // for none case
+}
+
+fn person_test() {
+    struct Person {
+        first: String,
+        middle: Option<String>,
+        last: String,
+    }
+
+    fn build_full_name(person: &Person) -> String {
+        let mut full_name = String::new();
+        full_name.push_str(&person.first);
+        full_name.push_str(" ");
+
+        // for middle in &person.middle {
+        //     full_name.push_str(middle);
+        //     full_name.push_str(" ")
+        // }
+
+        if let Some(middle) = &person.middle {
+            full_name.push_str(&middle);
+            full_name.push_str(" ")
+        }
+
+        full_name.push_str(&person.last);
+        full_name
+    }
+
+    fn main() {
+        let john = Person {
+            first: String::from("James"),
+            middle: Some(String::from("Oliver")),
+            last: String::from("Smith"),
+        };
+        assert_eq!(build_full_name(&john), "James Oliver Smith");
+
+        let alice = Person {
+            first: String::from("Alice"),
+            middle: None,
+            last: String::from("Stevens"),
+        };
+        assert_eq!(build_full_name(&alice), "Alice Stevens");
+
+        let bob = Person {
+            first: String::from("Robert"),
+            middle: Some(String::from("Murdock")),
+            last: String::from("Jones"),
+        };
+        assert_eq!(build_full_name(&bob), "Robert Murdock Jones");
+    }
+
+    main();
 }
