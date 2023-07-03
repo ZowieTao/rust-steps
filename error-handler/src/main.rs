1e3fef7504aae316a4a0cbbc6f8990e6a5828122
zowie <zowietao@gmail.com>
2023-07-03 14:41:18 +0800

    chore: safe division

--- error-handler/src/main.rs
+++ error-handler/src/main.rs
@@ -24,6 +24,8 @@ fn main() {
     unwrap_expect();
 
     person_test();
+
+    result_type_handle_err();
 }
 
 fn match_test() {
@@ -117,6 +119,27 @@ fn person_test() {
             last: String::from("Jones"),
         };
         assert_eq!(build_full_name(&bob), "Robert Murdock Jones");
+    }
+
+    main();
+}
+
+fn result_type_handle_err() {
+    #[derive(Debug)]
+    struct DivisionByZeroError;
+
+    fn safe_division(dividend: f64, divisor: f64) -> Result<f64, DivisionByZeroError> {
+        if divisor == 0.0 {
+            Err(DivisionByZeroError)
+        } else {
+            Ok(dividend / divisor)
+        }
+    }
+
+    fn main() {
+        println!("{:?}", safe_division(9.0, 3.0));
+        println!("{:?}", safe_division(4.0, 0.0));
+        println!("{:?}", safe_division(0.0, 2.0));
     }
 
     main();
