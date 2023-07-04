4eb51eddeb0d1eff2c29306c3e545461c9186f7b
zowietao <zowietao@gmail.com>
2023-07-05 04:28:54 +0800

    feat: define shared behavior with trait

--- generic-types-traits/src/main.rs
+++ generic-types-traits/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
     generic_data_type();
+
+    define_shared_behavior_with_trait();
 }
 
 fn generic_data_type() {
@@ -30,4 +32,41 @@ fn generic_data_type() {
         "{:?} {:?} {:?} {:?} {:?} {:?}",
         boolean.x, integer.y, float, string_slice, integer_and_boolean.x, integer_and_boolean.y
     )
+}
+
+fn define_shared_behavior_with_trait() {
+    trait Area {
+        fn area(&self) -> f64;
+    }
+
+    struct Circle {
+        radius: f64,
+    }
+
+    struct Rectangle {
+        width: f64,
+        height: f64,
+    }
+
+    impl Area for Circle {
+        fn area(&self) -> f64 {
+            use std::f64::consts::PI;
+            PI * self.radius.powf(2.0)
+        }
+    }
+
+    impl Area for Rectangle {
+        fn area(&self) -> f64 {
+            self.height * self.width
+        }
+    }
+
+    let circle = Circle { radius: 5.0 };
+    let rectangle = Rectangle {
+        width: 10.0,
+        height: 20.0,
+    };
+
+    println!("Circle area: {}", circle.area());
+    println!("Rectangle area: {}", rectangle.area());
 }
