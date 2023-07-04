90b332cd9f5d6ee81cd39ba9ca4cf4c9e605929a
zowietao <zowietao@gmail.com>
2023-07-05 00:45:45 +0800

    feat: exercise lifetimes

--- memory-manage/src/main.rs
+++ memory-manage/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     borrowed_test();
 
     validate_references_with_lifetimes();
+
+    exercise_lifetimes();
 }
 
 fn owner_clone_test() {
@@ -186,4 +188,26 @@ fn validate_references_with_lifetimes() {
         println!("{:?}", dog);
     }
     scene5()
+}
+
+fn exercise_lifetimes() {
+    fn copy_and_return<'a>(vector: &mut Vec<String>, value: &'a str) -> &'a str {
+        vector.push(String::from(value));
+        return value;
+    }
+
+    let name1 = "Joe";
+    let name2 = "Chris";
+    let name3 = "Anne";
+
+    let mut names = Vec::new();
+
+    assert_eq!("Joe", copy_and_return(&mut names, &name1));
+    assert_eq!("Chris", copy_and_return(&mut names, &name2));
+    assert_eq!("Anne", copy_and_return(&mut names, &name3));
+
+    assert_eq!(
+        names,
+        vec!["Joe".to_string(), "Chris".to_string(), "Anne".to_string()]
+    )
 }
