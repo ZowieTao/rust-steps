d9c4473368ae57a574364e0a7b2501ff4d078326
zowie <zowietao@gmail.com>
2023-07-05 14:51:56 +0800

    feat: exercise implement iterator

--- generic-types-traits/src/main.rs
+++ generic-types-traits/src/main.rs
@@ -12,6 +12,8 @@ fn main() {
     iterators();
 
     exercise_implement_generic_type();
+
+    exercise_implement_iterator();
 }
 
 fn generic_data_type() {
@@ -277,4 +279,84 @@ fn exercise_implement_generic_type() {
         assert_eq!(Container::new(Some("text")).value, Some("text"));
     }
     main();
+}
+
+fn exercise_implement_iterator() {
+    #[derive(Debug)]
+    struct Groups<T> {
+        inner: Vec<T>,
+    }
+
+    impl<T> Groups<T> {
+        fn new(inner: Vec<T>) -> Self {
+            Groups { inner }
+        }
+    }
+
+    impl<T: PartialEq> Iterator for Groups<T> {
+        type Item = Vec<T>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            // if the inner vector is empty, we are done
+            if self.inner.is_empty() {
+                return None;
+            }
+
+            // lets check the span of equal items
+            let mut cursor = 1;
+            let first = &self.inner[0];
+            for el in &self.inner[1..] {
+                if el == first {
+                    cursor += 1;
+                } else {
+                    break;
+                }
+            }
+
+            // we use the `Vec::drain` to extract items up until the cursor
+            let items = self.inner.drain(0..cursor).collect();
+
+            // return the extracted items
+            Some(items)
+        }
+    }
+
+    fn main() {
+        let data: Vec<i32> = vec![4, 1, 1, 2, 1, 3, 3, -2, -2, -2, 5, 5];
+        // groups:     |->|---->|->|->|--->|----------->|--->|
+        let group: Groups<i32> = Groups::new(data);
+        let iterator_result = group.into_iter().collect::<Vec<Vec<_>>>();
+
+        assert_eq!(
+            iterator_result,
+            vec![
+                vec![4],
+                vec![1, 1],
+                vec![2],
+                vec![1],
+                vec![3, 3],
+                vec![-2, -2, -2],
+                vec![5, 5],
+            ]
+        );
+
+        let data2: Vec<i32> = vec![1, 2, 2, 1, 1, 2, 2, 3, 4, 4, 3];
+        // groups:      |->|---->|---->|----|->|----->|->|
+        assert_eq!(
+            Groups::new(data2).into_iter().collect::<Vec<Vec<_>>>(),
+            vec![
+                vec![1],
+                vec![2, 2],
+                vec![1, 1],
+                vec![2, 2],
+                vec![3],
+                vec![4, 4],
+                vec![3],
+            ]
+        );
+
+        println!("after iterate, the iterator_result: {:?}", iterator_result)
+    }
+
+    main()
 }
